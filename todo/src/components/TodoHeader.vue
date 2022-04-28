6de2b31bff100df8abdc89e18f8e1d575df84ccc
mmg <alsaudrl111@naver.com>
2022-04-29 00:58:11 +0900

    init-commit

---
+++ todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Do It! ToDo</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!--스타일 정의를 해당 컴포넌트에만 적용한다.-->
+<style scoped>
+ h1 {
+   color : #2F3B52;
+   font-weight: 900;
+   margin: 2.5rem 0 1.5rem;
+ }
+</style>
